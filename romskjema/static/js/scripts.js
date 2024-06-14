375e74939b37aac9d7ebfcf57eeff31fb91872de
Torbjørn Skårslette Stensrud <torbjornskarslettestensrud@tbs.local>
2024-06-14 21:43:47 +0200

    styling

--- romskjema/static/js/scripts.js
+++ romskjema/static/js/scripts.js
@@ -1,5 +1,28 @@
-var modeSwitch = document.querySelector('.mode-switch');
-modeSwitch.addEventListener('click', function () {                      
-  document.documentElement.classList.toggle('light');
- modeSwitch.classList.toggle('active');
+
+document.addEventListener('DOMContentLoaded', function () {
+  const modeSwitch = document.querySelector('.mode-switch');
+
+  function applyTheme(theme) {
+      if (theme === 'light') {
+          document.documentElement.classList.add('light');
+          modeSwitch.classList.add('active');
+      } else {
+          document.documentElement.classList.remove('light');
+          modeSwitch.classList.remove('active');
+      }
+  }
+
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+      applyTheme(savedTheme);
+  } else {
+      applyTheme('dark');
+  }
+
+  modeSwitch.addEventListener('click', function () {
+      document.documentElement.classList.toggle('light');
+      modeSwitch.classList.toggle('active');
+      let currentTheme = document.documentElement.classList.contains('light') ? 'light' : 'dark';
+      localStorage.setItem('theme', currentTheme);
+  });
 });
